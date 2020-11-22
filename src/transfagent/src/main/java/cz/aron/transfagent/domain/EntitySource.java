36a9ae7370932468b130d25a326af7fab441165f
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2020-11-22 12:02:06 +0100

    ArchivOn 6670 uprava DB modelu

---
+++ src/transfagent/src/main/java/cz/aron/transfagent/domain/EntitySource.java
@@ -0,0 +1,54 @@
+package cz.aron.transfagent.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "entity_source")
+public class EntitySource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "entity_source_id")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "entity_id", nullable = true)
+    private ArchivalEntity archivalEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "apusource_id", nullable = true)
+    private ApuSource apuSource;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ArchivalEntity getArchivalEntity() {
+        return archivalEntity;
+    }
+
+    public void setArchivalEntity(ArchivalEntity archivalEntity) {
+        this.archivalEntity = archivalEntity;
+    }
+
+    public ApuSource getApuSource() {
+        return apuSource;
+    }
+
+    public void setApuSource(ApuSource apuSource) {
+        this.apuSource = apuSource;
+    }
+
+}
