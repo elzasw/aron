666860750230ff098e642d11172f979f2bc1e868
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2020-12-01 14:19:42 +0100

    ArchivOn 6665 opravy podle připomínek

--- src/transfagent/src/main/java/cz/aron/transfagent/service/FileImportService.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/service/FileImportService.java
@@ -14,7 +14,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -49,6 +48,12 @@ public class FileImportService implements SmartLifecycle {
     @Value("${aron.inputFolder}")
     private String inputFolder;
 
+    private Path inputPath;
+
+    private Path processedPath;
+
+    private Path errorPath;
+
     /**
      * Monitorování vstupního adresáře
      * 
@@ -61,11 +66,7 @@ public class FileImportService implements SmartLifecycle {
             throw new RuntimeException("Input folder not exists.");
         }
 
-        Path input = Path.of(inputFolder).resolve("input");
-        Path processed = Path.of(inputFolder).resolve("processed");
-        Path error = Path.of(inputFolder).resolve("error");
-
-        Path direct = input.resolve("direct");
+        Path direct = inputPath.resolve("direct");
 
         // kontrola, zda adresář direct existuje
         if (Files.notExists(direct)) {
@@ -73,49 +74,60 @@ public class FileImportService implements SmartLifecycle {
             throw new RuntimeException("Direct folder in input folder not exists.");
         }
 
-        // zpracování souborů v adresáři direct
-        processDirectFolder(direct, error);
-        moveFolderTo(direct, processed.resolve("direct"));
+        processDirectFolder(direct);
     }
 
     /**
-     * Zpracování souborů v adresáři direct
-     * 
+     * Zpracování adresářů v adresáři direct
+     *
      * @param path adresář direct
      * @return true nebo false
      */
-    private void processDirectFolder(Path path, Path error) throws IOException {
-        File[] files = path.toFile().listFiles(new FilenameFilter() {
+    private void processDirectFolder(Path path) throws IOException {
+        File[] dirs = path.toFile().listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
-                // process apux.xml or apux-XY.xml files
-                if (name.startsWith("apux") && name.endsWith("xml")) {
-                    return true;
-                }
-                return false;
+                return dir.isDirectory();
             }
         });
-
-        if (files.length == 0 || files.length > 1) {
-            moveFolderTo(path, error);
-            throw new RuntimeException("The folder should contain only one apux...xml file");
+        // pokud je adresář prázdný, není co zpracovávat
+        if (dirs.length == 0) {
+            return;
         }
+        for (File dir : dirs) {
+            File[] files = dir.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    // process apux.xml or apux-XY.xml files
+                    if (name.startsWith("apux") && name.endsWith("xml")) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
 
-        byte[] xml = Files.readAllBytes(files[0].toPath());
-        try {
-            ApuSource apux = unmarshalApuSourceFromXml(xml);
-            List<Apu> apuList = apux.getApus().getApu();
-            Apu apuItem = apuList.get(0);
-            cz.aron.transfagent.domain.ApuSource apuSource = new cz.aron.transfagent.domain.ApuSource();
-            apuSource.setData(xml);
-            apuSource.setSourceType(SourceType.valueOf(apuItem.getType().toString()));
-            apuSource.setUuid(UUID.fromString(apuItem.getUuid()));
-            apuSource.setDeleted(false);
-            apuSource.setDateImported(ZonedDateTime.now());
-            apuSourceRepository.save(apuSource);
-        } catch (JAXBException e) {
-            moveFolderTo(path, error);
-            throw new RuntimeException("Failed to parse", e);
+            if (files.length == 0 || files.length > 1) {
+                moveFolderTo(dir.toPath(), errorPath);
+                throw new RuntimeException("The folder should contain only one apux...xml file");
+            }
+
+            byte[] xml = Files.readAllBytes(files[0].toPath());
+            try {
+                ApuSource apux = unmarshalApuSourceFromXml(xml);
+                List<Apu> apuList = apux.getApus().getApu();
+                Apu apuItem = apuList.get(0);
+                cz.aron.transfagent.domain.ApuSource apuSource = new cz.aron.transfagent.domain.ApuSource();
+                apuSource.setData(xml);
+                apuSource.setSourceType(SourceType.valueOf(apuItem.getType().toString()));
+                apuSource.setUuid(UUID.fromString(apuItem.getUuid()));
+                apuSource.setDeleted(false);
+                apuSource.setDateImported(ZonedDateTime.now());
+                apuSourceRepository.save(apuSource);
+            } catch (JAXBException e) {
+                moveFolderTo(dir.toPath(), errorPath);
+                throw new RuntimeException("Failed to parse", e);
+            }
+            moveFolderTo(dir.toPath(), processedPath);
         }
     }
 
@@ -138,19 +150,19 @@ public class FileImportService implements SmartLifecycle {
     }
 
     /**
-     * Přesunutí adresáře do jiného adresáře
+     * Přesunutí adresáře se soubory do jiného adresáře
      * 
      * @param source
      * @param target
      * @throws IOException
      */
     private void moveFolderTo(Path source, Path target) throws IOException {
-        if (Files.exists(target)) {
+        Path targetFolder = target.resolve(source.getFileName());
+        if (Files.exists(targetFolder)) {
             String dateTime = new SimpleDateFormat("_yyyy_MM_dd_HH_mm_ss").format(new Date());
-            target = Path.of(target.toString() + dateTime);
+            targetFolder = Path.of(target.toString() + dateTime);
         }
-        Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
-        Files.createDirectories(source);
+        Files.move(source, targetFolder, StandardCopyOption.REPLACE_EXISTING);
     }
 
     public void run() {
@@ -167,6 +179,11 @@ public class FileImportService implements SmartLifecycle {
     @Override
     public void start() {
         status = ThreadStatus.RUNNING;
+
+        inputPath = Path.of(inputFolder).resolve("input");
+        processedPath = Path.of(inputFolder).resolve("processed");
+        errorPath = Path.of(inputFolder).resolve("error");
+
         new Thread(() -> {
             run();
         }).start();
@@ -181,13 +198,4 @@ public class FileImportService implements SmartLifecycle {
     public boolean isRunning() {
         return status == ThreadStatus.RUNNING;
     }
-
-    public static JAXBContext createJaxbContext(Class<?>... classesToBeBound) {
-        try {
-            return JAXBContext.newInstance(classesToBeBound);
-        } catch (JAXBException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
