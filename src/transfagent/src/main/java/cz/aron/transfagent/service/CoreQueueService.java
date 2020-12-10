8b257ecbda564bc653851652ab41ee3ae930a6a0
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2020-12-10 17:13:49 +0100

    ArchivOn 6668 pokus o použití fileTransfer

--- src/transfagent/src/main/java/cz/aron/transfagent/service/CoreQueueService.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/service/CoreQueueService.java
@@ -6,6 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.stereotype.Service;
 
+import com.lightcomp.ft.FileTransfer;
+import com.lightcomp.ft.client.Client;
+import com.lightcomp.ft.client.ClientConfig;
+import com.lightcomp.ft.client.UploadRequest;
+import com.lightcomp.ft.simple.UploadRequestImpl;
+import com.lightcomp.ft.xsd.v1.GenericDataType;
+
+import cz.aron.transfagent.config.ConfigAronCore;
 import cz.aron.transfagent.domain.CoreQueue;
 import cz.aron.transfagent.repository.CoreQueueRepository;
 
@@ -17,18 +25,42 @@ public class CoreQueueService implements SmartLifecycle {
     @Autowired
     CoreQueueRepository coreQueueRepository;
 
+    @Autowired
+    ConfigAronCore configAronCore;
+
     private ThreadStatus status;
 
     /**
-     * Odeslání pomocí WSDL data do jádra
+     * Odeslání dat do jádra
      */
     private void sendData() {
         if (coreQueueRepository.count() > 0) {
             CoreQueue item = coreQueueRepository.findFirstByOrderById();
-
-            // TODO send to core
-
+            uploadData(item);
             coreQueueRepository.delete(item);
+        }
+    }
+
+    /**
+     * Přenos dat pomocí FileTransfer
+     * 
+     * @param item
+     */
+    private void uploadData(CoreQueue item) {
+
+        // vytváření Clienta
+        ClientConfig clientConfig = new ClientConfig(configAronCore.getUrl());
+        clientConfig.setSoapLogging(configAronCore.getSoapLogging());
+
+        Client client = FileTransfer.createClient(clientConfig);
+
+        // vytváření Requesta
+        GenericDataType dataType = new GenericDataType();
+        UploadRequest request = new UploadRequestImpl(clientConfig.getWorkDir(), dataType);
+        try {
+            client.uploadSync(request);
+        } finally {
+            client.stop();
         }
     }
 
