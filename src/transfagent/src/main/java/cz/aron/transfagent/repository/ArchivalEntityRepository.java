6d45cd2f41a148bdd702c50cd976b640bc97b496
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2020-12-14 10:56:10 +0100

    ArchivOn 6669 implementace služby pro příjem požadavků

--- src/transfagent/src/main/java/cz/aron/transfagent/repository/ArchivalEntityRepository.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/repository/ArchivalEntityRepository.java
@@ -1,11 +1,17 @@
 package cz.aron.transfagent.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import cz.aron.transfagent.domain.ArchivalEntity;
+import cz.aron.transfagent.domain.EntityStatus;
 
 @Repository
 public interface ArchivalEntityRepository extends JpaRepository<ArchivalEntity, Integer> {
+
+    List<ArchivalEntity> findByUuidInAndStatus(List<UUID> uuids, EntityStatus status);
 
 }
---
+++ src/transfagent/src/main/java/cz/aron/transfagent/service/ApuTransformManagerService.java
@@ -0,0 +1,57 @@
+package cz.aron.transfagent.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cz.aron.apux._2020.UuidList;
+import cz.aron.transfagent.domain.ArchivalEntity;
+import cz.aron.transfagent.domain.CoreQueue;
+import cz.aron.transfagent.domain.EntityStatus;
+import cz.aron.transfagent.repository.ArchivalEntityRepository;
+import cz.aron.transfagent.repository.CoreQueueRepository;
+import cz.aron.transform_agent.v1.ApuTransformManager;
+
+@Service
+public class ApuTransformManagerService implements ApuTransformManager {
+
+    private static final Logger log = LoggerFactory.getLogger(ApuTransformManagerService.class);
+
+    @Autowired
+    ArchivalEntityRepository archivalEntityRepository;
+
+    @Autowired
+    CoreQueueRepository coreQueueRepository;
+
+    @Override
+    public void requestApus(UuidList requestApus) {
+        Validate.notNull(requestApus, "The request must not be null");
+
+        List<UUID> uuids = requestApus.getUuid().stream()
+                .map(p -> UUID.fromString(p))
+                .collect(Collectors.toList());
+
+        if (!uuids.isEmpty()) {
+            List<ArchivalEntity> archivalEntities = archivalEntityRepository.findByUuidInAndStatus(uuids,
+                                                                                                   EntityStatus.AVAILABLE);
+            if (!archivalEntities.isEmpty()) {
+                List<CoreQueue> coreQueues = new ArrayList<>(archivalEntities.size());
+                for (ArchivalEntity entita : archivalEntities) {
+                    CoreQueue coreQueue = new CoreQueue();
+                    coreQueue.setApuSource(entita.getApuSource());
+                }
+
+                coreQueueRepository.saveAll(coreQueues);
+                log.info("Sent to CoreQueue {} record(s).", coreQueues.size());
+            }
+        }
+    }
+
+}
