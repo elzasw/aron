1d62f8a6ee24e72632d05207d5c1c4d3be5674c3
Karel Žáček <zacek@lightcomp.cz>
2020-12-17 16:25:46 +0100

    inicializace webove sluzby ApuTransformManager

--- src/transfagent/src/main/java/cz/aron/transfagent/service/WebServiceConfig.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/service/WebServiceConfig.java
@@ -2,31 +2,44 @@ package cz.aron.transfagent.service;
 
 import javax.xml.ws.Endpoint;
 
+import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource({"classpath:META-INF/cxf/cxf.xml", "classpath:META-INF/cxf/cxf-servlet.xml"})
 public class WebServiceConfig {
 
 	public final static String TRANSFORM_AGENT_MANAGER_URL = "/ApuTransformManager";
 	
 	private final ApuTransformManagerService apuTransformManagerService;
 	    
-    private final SpringBus bus;	
-	
-	public WebServiceConfig(final ApuTransformManagerService apuTransformManagerService,
-							final SpringBus bus) {
+		
+	public WebServiceConfig(final ApuTransformManagerService apuTransformManagerService) {
 		this.apuTransformManagerService = apuTransformManagerService;
-		this.bus = bus;
 	}
 	
+    @Bean(name = Bus.DEFAULT_BUS_ID)
+    public SpringBus springBus() {
+        return new SpringBus();
+    }
+
+	
     @Bean
-    public Endpoint daoDigitizationServiceEndpoint() {
-        EndpointImpl endpoint = new EndpointImpl(bus, apuTransformManagerService);
+    public ServletRegistrationBean<CXFServlet> cxfServlet() {
+        return new ServletRegistrationBean<CXFServlet>(new CXFServlet(), "/ws/*");
+    }
+
+	
+    @Bean
+    public Endpoint apuTransformManagerServiceEndpoint() {
+        EndpointImpl endpoint = new EndpointImpl(springBus(), apuTransformManagerService);
+        String wsdlLocation = WebServiceConfig.class.getResource("/wsdl/aron_transform_agent.wsdl")
+                .toExternalForm();
+        endpoint.setWsdlLocation(wsdlLocation);
         endpoint.publish(TRANSFORM_AGENT_MANAGER_URL);
         return endpoint;
     }
