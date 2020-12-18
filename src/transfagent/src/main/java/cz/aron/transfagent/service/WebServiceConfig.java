649d14fb5c6f5fd3617c6f67e28713aca8ba1804
Karel Žáček <zacek@lightcomp.cz>
2020-12-18 11:45:31 +0100

    soapLogging na ws endpointu

--- src/transfagent/src/main/java/cz/aron/transfagent/service/WebServiceConfig.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/service/WebServiceConfig.java
@@ -4,8 +4,10 @@ import javax.xml.ws.Endpoint;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,23 +18,24 @@ public class WebServiceConfig {
 	public final static String TRANSFORM_AGENT_MANAGER_URL = "/ApuTransformManager";
 	
 	private final ApuTransformManagerService apuTransformManagerService;
-	    
-		
-	public WebServiceConfig(final ApuTransformManagerService apuTransformManagerService) {
-		this.apuTransformManagerService = apuTransformManagerService;
-	}
+	
+	private final boolean soapLogging;
+	    		
+    public WebServiceConfig(final ApuTransformManagerService apuTransformManagerService,
+            @Value("${aron.soapLogging:false}") boolean soapLogging) {
+        this.apuTransformManagerService = apuTransformManagerService;
+        this.soapLogging = soapLogging;
+    }
 	
     @Bean(name = Bus.DEFAULT_BUS_ID)
     public SpringBus springBus() {
         return new SpringBus();
     }
-
 	
     @Bean
     public ServletRegistrationBean<CXFServlet> cxfServlet() {
         return new ServletRegistrationBean<CXFServlet>(new CXFServlet(), "/ws/*");
     }
-
 	
     @Bean
     public Endpoint apuTransformManagerServiceEndpoint() {
@@ -41,6 +44,13 @@ public class WebServiceConfig {
                 .toExternalForm();
         endpoint.setWsdlLocation(wsdlLocation);
         endpoint.publish(TRANSFORM_AGENT_MANAGER_URL);
+
+        if (soapLogging) {
+            LoggingFeature logFeature = new LoggingFeature();
+            logFeature.setPrettyLogging(true);
+            logFeature.initialize(springBus());
+            endpoint.getFeatures().add(logFeature);
+        }
         return endpoint;
     }
 	
