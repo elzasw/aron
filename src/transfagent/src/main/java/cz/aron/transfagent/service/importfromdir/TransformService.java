e30f2234ec736ffe94bf0c5716e7e358119cd344
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2021-02-17 14:36:58 +0100

    ArchivOn - drobná oprava v TransformService

--- src/transfagent/src/main/java/cz/aron/transfagent/service/importfromdir/TransformService.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/service/importfromdir/TransformService.java
@@ -242,7 +242,8 @@ public class TransformService {
             FilesArchiver archiver = new DirectoryArchiver(tempDir.toFile());
             PartialImageReader pir = new BufferedImageReader(sourceImage.toFile());
             spb.buildPyramid(pir, "image", archiver, 1);
-            try (ZipOutputStream outputStream = new ZipOutputStream(Files.newOutputStream(targetFile, StandardOpenOption.CREATE))) {
+            Files.createFile(targetFile);
+            try (ZipOutputStream outputStream = new ZipOutputStream(Files.newOutputStream(targetFile))) {
                 Files.walkFileTree(tempDir, new SimpleFileVisitor<Path>() {
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
