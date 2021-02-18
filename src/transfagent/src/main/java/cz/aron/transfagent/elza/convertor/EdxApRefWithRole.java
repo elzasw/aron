9985e605766d8a61540640fd9a4726842abc9117
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2021-02-18 11:25:39 +0100

    ArchivOn 6931 uprava prevodu ElzaTypes.ZP2015_ENTITY_ROLE (EdxApRefWithRole)

--- src/transfagent/src/main/java/cz/aron/transfagent/elza/convertor/EdxApRefWithRole.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/elza/convertor/EdxApRefWithRole.java
@@ -9,16 +9,17 @@ import cz.aron.apux.ApuSourceBuilder;
 import cz.aron.apux._2020.Part;
 import cz.aron.transfagent.elza.ElzaXmlReader;
 import cz.aron.transfagent.transformation.ContextDataProvider;
+import cz.aron.transfagent.transformation.CoreTypes;
 import cz.tacr.elza.schema.v2.AccessPoint;
 import cz.tacr.elza.schema.v2.DescriptionItem;
 import cz.tacr.elza.schema.v2.DescriptionItemAPRef;
 import cz.tacr.elza.schema.v2.DescriptionItemUndefined;
 
-public class EdxApRefWithRole  implements EdxItemConvertor {
+public class EdxApRefWithRole implements EdxItemConvertor {
 
-	private final  String partType;
-	
-	private final ContextDataProvider dataProvider;
+    private final String partType;
+
+    private final ContextDataProvider dataProvider;
     private Map<String, String> specMap;
 
     public EdxApRefWithRole(String partType,
@@ -29,39 +30,45 @@ public class EdxApRefWithRole  implements EdxItemConvertor {
         this.specMap = specMap;
     }
 
-	@Override
-	public void convert(EdxItemCovertContext ctx, DescriptionItem item) {
-		if(item instanceof DescriptionItemUndefined) {
-			return;
-		}
-		DescriptionItemAPRef apRef = (DescriptionItemAPRef)item;
-		
-		ElzaXmlReader elzaXmlReader = ctx.getElzaXmlReader();
-		Map<String, AccessPoint> apMap = elzaXmlReader.getApMap();
-		AccessPoint ap = apMap.get(apRef.getApid());
-		if(ap==null) {
-			throw new RuntimeException("Failed to convert AP: "+apRef.getApid() + ", ap not found");
-		}
-		
+    @Override
+    public void convert(EdxItemCovertContext ctx, DescriptionItem item) {
+        if(item instanceof DescriptionItemUndefined) {
+            return;
+        }
+        DescriptionItemAPRef apRef = (DescriptionItemAPRef)item;
+
+        ElzaXmlReader elzaXmlReader = ctx.getElzaXmlReader();
+        Map<String, AccessPoint> apMap = elzaXmlReader.getApMap();
+        AccessPoint ap = apMap.get(apRef.getApid());
+        if(ap==null) {
+            throw new RuntimeException("Failed to convert AP: " + apRef.getApid() + ", ap not found");
+        }
+
         ApuSourceBuilder apusBuilder = ctx.getApusBuilder();
-        Part part = ApuSourceBuilder.addPart(ctx.getActiveApu(), partType);
+        Part part = null;
+        if(partType.equals(CoreTypes.PT_ENTITY_ROLE)) {
+            part = ApuSourceBuilder.getFirstPart(ctx.getActiveApu(), partType);
+        }
+        if (part == null) {
+            part = ApuSourceBuilder.addPart(ctx.getActiveApu(), partType);
+        }
 
         UUID apUuid = UUID.fromString(ap.getApe().getUuid());
-		var uuids = dataProvider.findByUUIDWithParents(apUuid);
-		
-		String t = this.specMap.get(apRef.getS());
-		if(t==null) {
-		    throw new RuntimeException("Missing mapping for type: "+apRef.getT()+", spec: "+apRef.getS() + ", ap not found");
-		}
-		
-		//apusBuilder.addEnum(part, roleType, s, true);		
+        var uuids = dataProvider.findByUUIDWithParents(apUuid);
+
+        String t = this.specMap.get(apRef.getS());
+        if(t==null) {
+            throw new RuntimeException("Missing mapping for type: " + apRef.getT() + ", spec: " + apRef.getS() + ", ap not found");
+        }
+
+        //apusBuilder.addEnum(part, roleType, s, true);
         if (CollectionUtils.isEmpty(uuids)) {
             ctx.addArchEntityRef(apUuid);
             apusBuilder.addApuRef(part, t, apUuid);
         } else {
             uuids.forEach(uuid -> ctx.addArchEntityRef(uuid));
             apusBuilder.addApuRefsFirstVisible(part, t, uuids);
-        }		
-	}
+        }
+    }
 
 }
