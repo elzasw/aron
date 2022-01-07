cfbb5931230c1243225b0f6244926715f44cf002
Karel Žáček <zacek@lightcomp.cz>
2022-01-07 15:31:13 +0100

    transormagent - datace puvodcu dd.mm.yyyy/dd.mm.yyyy

--- src/transfagent/src/main/java/cz/aron/transfagent/peva/Peva2Utils.java
+++ src/transfagent/src/main/java/cz/aron/transfagent/peva/Peva2Utils.java
@@ -119,45 +119,106 @@ public class Peva2Utils {
 			return camCode;
 		}
 	}
-	
-	
-	private static Pattern DATE_PATTERN = Pattern.compile("^(\\d{1,2}).(\\d{1,2}).(\\d+)$");
+
+	private static Pattern DATE_PATTERN = Pattern.compile("^(\\d{1,2})\\.(\\d{1,2})\\.(\\d+)$");
 	private static Pattern DATE_PATTERN_YEAR = Pattern.compile("^\\d+$");
 	
 	/**
-	 * Prevede datum z formatu dd.mm.yyyy na ItemDateRange
+	 * Prevede datum z formatu [dd.mm.]yyyy nebo [dd.mm.]yyyy/[dd.mm.]yyyy na ItemDateRange
 	 * @param date
-	 * @param type typ nastavovany 
-	 * @return
-	 */
+	 * @param type typ nastavovany do vraceneho ItemDateRange
+	 * @return ItemDateRange
+	 */	
 	public static ItemDateRange parseDating(String date, String type) {
 		var idr = new ItemDateRange();
 		idr.setType(type);
-		var matcher = DATE_PATTERN_YEAR.matcher(date);
-		if (matcher.matches()) {
-			var localDate = LocalDate.of(Integer.parseInt(matcher.group(0)), 1, 1);
-			idr.setFmt("Y-Y");
-			idr.setF(localDate.with(TemporalAdjusters.firstDayOfYear()).atTime(0,0,0).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-			idr.setFe(false);
-			idr.setTo(localDate.with(TemporalAdjusters.lastDayOfYear()).atTime(23, 59, 59).toString());
-			idr.setToe(false);
-			idr.setVisible(true);
+		idr.setVisible(true);
+		var splitted = date.split("/");
+		var format = new StringBuilder();
+		if (splitted.length == 1) {
+			if (!fillDate(splitted[0], idr, true, true, format)) {
+				return null;
+			}
+			idr.setFmt(format.toString());
 			return idr;
-		}
-		matcher = DATE_PATTERN.matcher(date);
-		if (matcher.matches()) {
-			var localDate = LocalDate.of(Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(2)),
-					Integer.parseInt(matcher.group(1)));
-			idr.setFmt("D-D");
-			idr.setF(localDate.atTime(0,0,0).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-			idr.setFe(false);
-			idr.setTo(localDate.atTime(23, 59, 59).toString());
-			idr.setToe(false);
-			idr.setVisible(true);
+		} else if (splitted.length == 2) {
+			if (!fillDate(splitted[0], idr, true, false, format)) {
+				return null;
+			}
+			if (!fillDate(splitted[1], idr, false, true, format)) {
+				return null;
+			}
+			idr.setFmt(format.toString());
 			return idr;
 		}
 		return null;
 	}
 
+	private static boolean fillDate(String date, ItemDateRange idr, boolean fillStart, boolean fillEnd, StringBuilder formatBuilder) {
+		var localDate = tryParseYearDate(date);
+		if (localDate != null) {
+			if (fillStart) {
+				setStartYear(localDate, idr);
+				formatBuilder.append("Y");
+			}
+			if (fillEnd) {
+				setEndYear(localDate, idr);
+				formatBuilder.append("-Y");
+			}
+			return true;
+		} else {
+			localDate = tryParseYearMonthDayDate(date);
+			if (localDate != null) {
+				if (fillStart) {
+					setStartYearMonthDay(localDate, idr);
+					formatBuilder.append("D");
+				}
+				if (fillEnd) {
+					setEndYearMonthDay(localDate, idr);
+					formatBuilder.append("-D");
+				}
+				return true;
+			}
+		}
+		return false;
+	}
 	
+	private static void setStartYear(LocalDate localDate, ItemDateRange idr) {
+		idr.setF(localDate.with(TemporalAdjusters.firstDayOfYear()).atTime(0, 0, 0)
+				.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+		idr.setFe(false);
+	}
+
+	private static void setEndYear(LocalDate localDate, ItemDateRange idr) {
+		idr.setTo(localDate.with(TemporalAdjusters.lastDayOfYear()).atTime(23, 59, 59).toString());
+		idr.setToe(false);
+	}	
+	
+	private static void setStartYearMonthDay(LocalDate localDate, ItemDateRange idr) {
+		idr.setF(localDate.atTime(0, 0, 0).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+		idr.setFe(false);
+	}
+	
+	private static void setEndYearMonthDay(LocalDate localDate, ItemDateRange idr) {
+		idr.setTo(localDate.atTime(23, 59, 59).toString());
+		idr.setToe(false);
+	}
+	
+	private static LocalDate tryParseYearDate(String date) {
+		var matcher = DATE_PATTERN_YEAR.matcher(date);
+		if (matcher.matches()) {
+			return LocalDate.of(Integer.parseInt(matcher.group(0)), 1, 1);
+		}
+		return null;
+	}
+	
+	private static LocalDate tryParseYearMonthDayDate(String date) {
+		var matcher = DATE_PATTERN.matcher(date);
+		if (matcher.matches()) {
+			return LocalDate.of(Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(2)),
+					Integer.parseInt(matcher.group(1)));
+		}
+		return null;
+	}
+
 }
--- src/transfagent/src/test/java/cz/aron/transfagent/peva/Peva2UtilsTest.java
+++ src/transfagent/src/test/java/cz/aron/transfagent/peva/Peva2UtilsTest.java
@@ -18,14 +18,19 @@ public class Peva2UtilsTest {
 	
 	@Test
 	public void testParseDating() {		
-		compareIdr(Peva2Utils.parseDating("1.1.2000", "type"),"2000-01-01T00:00:00","2000-01-01T23:59:59");				
-		compareIdr(Peva2Utils.parseDating("1928", "type"),"1928-01-01T00:00:00","1928-12-31T23:59:59");
-		compareIdr(Peva2Utils.parseDating("1.2.1996", "type"),"1996-02-01T00:00:00","1996-02-01T23:59:59");
+		compareIdr(Peva2Utils.parseDating("1.1.2000", "type"),"2000-01-01T00:00:00","2000-01-01T23:59:59","D-D");				
+		compareIdr(Peva2Utils.parseDating("1928", "type"),"1928-01-01T00:00:00","1928-12-31T23:59:59","Y-Y");
+		compareIdr(Peva2Utils.parseDating("1.2.1996", "type"),"1996-02-01T00:00:00","1996-02-01T23:59:59","D-D");
+		compareIdr(Peva2Utils.parseDating("1990/2000", "type"),"1990-01-01T00:00:00","2000-12-31T23:59:59","Y-Y");
+		compareIdr(Peva2Utils.parseDating("1.1.1990/2000", "type"),"1990-01-01T00:00:00","2000-12-31T23:59:59","D-Y");
+		compareIdr(Peva2Utils.parseDating("1990/1.1.2000", "type"),"1990-01-01T00:00:00","2000-01-01T23:59:59","Y-D");
+		compareIdr(Peva2Utils.parseDating("1.1.1990/1.1.2000", "type"),"1990-01-01T00:00:00","2000-01-01T23:59:59","D-D");
 	}
 	
-	private void compareIdr(ItemDateRange idr, String f, String t) {
-		assertEquals(idr.getF(),f);
-		assertEquals(idr.getTo(),t);
+	private void compareIdr(ItemDateRange idr, String f, String t, String fmt) {
+		assertEquals(idr.getF(), f);
+		assertEquals(idr.getTo(), t);
+		assertEquals(idr.getFmt(), fmt);
 	}
 	
 }
