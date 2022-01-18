90b82a5760169f02c6e3e4f82b1fd100dc789352
Jakub Randák <jakub.randak@lightcomp.cz>
2022-01-18 12:34:41 +0100

    aron-web - pridani scrollovani cele stranky v mobilnim zobrazeni

--- src/badatelna/aron-web/src/components/app-wrapper/styles.ts
+++ src/badatelna/aron-web/src/components/app-wrapper/styles.ts
@@ -1,20 +1,27 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles((theme) => ({
-  appWrapper: {
-    width: '100vw',
-    minWidth: '100vw',
-    maxWidth: '100vw',
-    height: '100%',
-    // minHeight: '100vh',
-    overflowY: 'hidden',
-    background: '#fff',
-    overflowX: 'hidden',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-
-  iOSWrapper: {
-    paddingBottom: '15vh',
-  },
-}));
+export const useStyles = makeStyles((theme) => {
+  const md = theme.breakpoints.up('md');
+  return {
+    appWrapper: {
+      width: '100vw',
+      minWidth: '100vw',
+      maxWidth: '100vw',
+      // minHeight: '100vh',
+      overflowY: 'auto',
+      background: '#fff',
+      overflowX: 'hidden',
+      display: 'flex',
+      flexDirection: 'column',
+   
+      [md]: {
+        overflowY: 'hidden',
+        height: '100%',
+      },
+    },
+   
+    iOSWrapper: {
+      paddingBottom: '15vh',
+    },
+  }
+});
